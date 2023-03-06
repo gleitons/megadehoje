7595b47a9598fc89e7350ed8463c53c051f3c20c
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2023-03-06 08:02:34 -0300

    monte

--- Components/Anuncios.jsx
+++ Components/Anuncios.jsx
@@ -1,12 +1,16 @@
 import styles from '../styles/Anuncio.module.css'
+import Image from 'next/image'
+import asConstrutora from '../public/img/as-construtora-lagoa-dos-patos-mg.jpeg'
 export default function Anuncios() {
     return (
         <>
-            <p>Anuncios</p>
+            {/* <p>Anuncios</p> */}
             <div className={styles.anuncio600x300}>
-                anuncio 600x300
+            <Image src={asConstrutora} alt="AS Construtora" />
+               
             </div>
             <div className={styles.anuncio300x250}>
+           
                 anuncio 300x250
             </div>
 
